93451e157f9d4509891da972018935c8a115c721
localhostdevtest <localhostdevtest@outlook.com>
2026-04-01 15:13:02 -0300

    ok

--- src/visitor/dto/create-visitor.dto.ts
+++ src/visitor/dto/create-visitor.dto.ts
@@ -7,7 +7,7 @@ export class CreateVisitorDto {
     description: 'Property UUID',
     example: '123e4567-e89b-12d3-a456-426614174000',
   })
-  @IsUUID()
+  @IsString()
   property: string;
 
   @ApiProperty({
